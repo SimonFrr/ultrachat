7965eec782c8b322f64433473f5ca80e143195fd
SimonFrr <sim.frr@gmail.com>
2017-09-22 19:54:34 +0200

    display messags from db

--- client/main.js
+++ client/main.js
@@ -1,11 +1,10 @@
 import { Template } from 'meteor/templating';
+import { Messages } from '../imports/api/messages.js';
 
 import './main.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  messages() {
+    return Messages.find({});
+  }
 });
