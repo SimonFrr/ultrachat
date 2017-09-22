840031c554b61647615b314626a06ace8e74e38c
SimonFrr <sim.frr@gmail.com>
2017-09-22 20:55:47 +0200

    main functionalities

--- client/main.js
+++ client/main.js
@@ -8,3 +8,36 @@ Template.body.helpers({
     return Messages.find({});
   }
 });
+
+Template.body.events({
+  'submit #new-message'(event) {
+    console.log("'bla'");
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const name = target.name.value;
+    let text = target.text.value;
+    text = modify(text);
+
+    // Insert a task into the collection
+    Messages.insert({
+      name,
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
+
+const modify = text => {
+  return text
+    .replace(/a/g, "")
+    .replace(/e/g, "")
+    .replace(/i/g, "")
+    .replace(/o/g, "")
+    .replace(/u/g, "")
+}
